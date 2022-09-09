3177bdc8827fb6eb8d0e52afb3c3c252671140b5
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-09-09 11:56:06 +0530

    import java.util.Scanner; class area {     public static void main(String args[])     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=(a*(a-1)/2);         System.out.println(b);         sc.close();
        }
        
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class area
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=(a*(a-1)/2);
+        System.out.println(b);
+        sc.close();
+        
+    }
+    
+}
