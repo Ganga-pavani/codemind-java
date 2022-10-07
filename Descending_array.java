f8afebb6811294149b7c4ce5e9e92cc8fa2feb11
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-10-07 14:31:51 +0530

    import java.util.Scanner; class des {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int c=0;         int [] arr=new int[50];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             if(arr[i]>arr[i+1])             {                 c+=1;             }         }         if(c==n-1)         {             System.out.println("no");         }         else             System.out.println("yes");     } }

---
+++ Descending_array.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class des
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=0;
+        int [] arr=new int[50];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                c+=1;
+            }
+        }
+        if(c==n-1)
+        {
+            System.out.println("no");
+        }
+        else
+            System.out.println("yes");
+    }
+}
