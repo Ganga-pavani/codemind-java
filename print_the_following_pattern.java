820c8c892189b93434e9d5e3b0441509f25b9662
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-10-14 17:58:42 +0530

    import java.util.Scanner; class pat {     public static void main(String args[])     {        Scanner sc=new Scanner(System.in);        int n=sc.nextInt();        for(int i=1;i<=n;i++)        {            for(int j=1;j<=i;j++)            {                System.out.print(j);            }            System.out.println();        }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
-class Star
+class pat
 {
     public static void main(String args[])
     {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=n;i>=1;i--)
-        {
-            for(int j=1;j<=i;j++)
-            {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=i;j++)
+           {
+               System.out.print(j);
+           }
+           System.out.println();
+       }
     }
 }
