063a33c61be663166b06ac83aee90b97414c4752
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-10-14 18:24:25 +0530

    import java.util.Scanner; class pat {     public static void main(String args[])     {        Scanner sc=new Scanner(System.in);        int n=sc.nextInt();        for(int i=1;i<=n;i++)        {            for(int j=i;j<=n;j++)            {                System.out.print(j+" ");            }            System.out.println();        }     } }

--- Print_the_following_pattern.java
+++ Print_the_following_pattern.java
@@ -5,18 +5,11 @@ class pat
     {
        Scanner sc=new Scanner(System.in);
        int n=sc.nextInt();
-       for(int i=n;i>=1;i--)
+       for(int i=1;i<=n;i++)
        {
-           for(int j=n;j>=1;j--)
+           for(int j=i;j<=n;j++)
            {
-               if(i==j||j==(n-i+1))
-               {
-                  System.out.print(i+" ");
-               }
-               else
-               {
-                   System.out.print(" ");
-               }
+               System.out.print(j+" ");
            }
            System.out.println();
        }
