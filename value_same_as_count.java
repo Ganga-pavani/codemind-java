e70fc908fad341da8d0a0d5b88269ffb417b0794
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-11-01 14:39:33 +0530

    import java.util.Scanner; class ele {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int y=0;         int count=0;         int [] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             int c=1;             if(arr[i]==-1)             {                 continue;             }             for(int j=i+1;j<n;j++)             {                 if(arr[i]==arr[j])                 {                     c++;                     arr[j]=-1;                 }             }             if (c==arr[i])             {                 count++;             }         }         /*if(y==0)         {             System.out.print("-1");         }*/
                System.out.println(count);
        
        }
    }

---
+++ value_same_as_count.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class ele
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int y=0;
+        int count=0;
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            int c=1;
+            if(arr[i]==-1)
+            {
+                continue;
+            }
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    c++;
+                    arr[j]=-1;
+                }
+            }
+            if (c==arr[i])
+            {
+                count++;
+            }
+        }
+        /*if(y==0)
+        {
+            System.out.print("-1");
+        }*/
+    
+            System.out.println(count);
+    
+    }
+}
