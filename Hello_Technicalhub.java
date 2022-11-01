21db18aeed44581464aa3e1733fcade9fbd0573f
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-11-01 22:03:39 +0530

    import java.util.Scanner; import java.lang.*; class words {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         System.out.println("Hello Technicalhub");         System.out.print(s);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.lang.*;
+class words
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.print(s);
+    }
+}
