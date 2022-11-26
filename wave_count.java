88e6acfe2d3e147b1fd157bff85e474ea8f42640
Ganga-pavani <100119912+Ganga-pavani@users.noreply.github.com>
2022-11-26 10:36:05 +0530

    import java.util.Scanner; class wave {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int c=0;         for(int i=0;i<n-2;i+=2)         {             if(a[i+1]>a[i] && a[i+1]>a[i+2])             {                 c=c+1;             }             else if(a[i]>a[i+1])             {                 c=-1;                 break;             }         }         System.out.println(c);     } }

---
+++ wave_count.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class wave
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int c=0;
+        for(int i=0;i<n-2;i+=2)
+        {
+            if(a[i+1]>a[i] && a[i+1]>a[i+2])
+            {
+                c=c+1;
+            }
+            else if(a[i]>a[i+1])
+            {
+                c=-1;
+                break;
+            }
+        }
+        System.out.println(c);
+    }
+}
